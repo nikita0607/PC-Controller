b0022812a56996bdd76deb26ed127dd221c75e3d
nikita0607 <37401771+nikita0607@users.noreply.github.com>
2021-12-09 00:49:08 +0800

    Add new methods
    Add event listening:
        Add new class Event()
        Add new class EventValue()
        Add Event instance: ButtonEvent(Event)
        Add events and buttons to Computer()
    
    Add logic for new methods

--- api_server/computer.py
+++ api_server/computer.py
@@ -3,6 +3,7 @@ from api_server.api_errors import APIError, NameBusy, UnknownComputer, WrongHash
 
 from connection import Connection
 from api_errors import *
+from database import Database
 
 from hashlib import sha256
 from random import randint
@@ -22,21 +23,47 @@ class Method:
     def __eq__(self, other):
         return str(self) == other
 
-    def validate(self, _dict: dict) -> dict | None:
+    def validate(self, _dict: dict) -> APIError | self:
         res = validate_dict(_dict, *self.required_values)
         return res if res else self
 
 
 class Methods:
     CONNECT = Method("computer.connect", "name", need_hash_key=False)
+    PING = Method("computer.ping", need_hash_key=False)
+    PING_ERROR = Method("computer.ping_error", need_hash_key=False)
+
+    BUTTON_ADD = Method("computer.button.add", "username", "name", "button_name", "button_text")
+    BUTTON_CLICK = Method("computer.button.click", "username", "name", "button_name")
 
     @classmethod
-    def find_and_validate(cls, _dict) -> dict | Method | None:
+    def find_and_validate(cls, _dict) -> APIError | Method | None:
         methods: list[Method] = [cls.__dict__[i] for i in cls.__dict__ if isinstance(cls.__dict__[i], Method)]
 
         for method in methods:
             if method.string == _dict["method"]:
                 return method.validate(_dict)
+
+
+class EventValue:
+    def __init__(self, default_value=None):
+        self.value = default_value
+
+    def __get__(self, *):
+        return self.value
+
+
+class Event:
+    def to_dict() -> dict:
+        return [i: getattr(self, i) for i in cls.__dict__ if isinstance(cls.__dict__[i], EventValue)]
+
+
+class ButtonClickEvent(Event):
+    def __init__(self):
+        self.click_count = EventValue(0)
+
+    def add_click(self):
+        self.click_count += 1
 
 
 class Computer:
@@ -45,10 +72,27 @@ class Computer:
         self.name = name
         self.hash_key = hash_key
 
+        self.buttons: dict[str, str] = {}
+        self.events = []
+
+    def add_button(self, name: str, text: str) -> None:
+        self.buttons[name] = text
+
+    def button_click(self, button_name: str) -> bool:
+        if button_name in self.buttons:
+            if len(self.events) and isinstance(self.events[-1], ButtonClickEvent):
+                self.events[-1].add_click()
+            else:
+                self.events.append(ButtonClickEvent())
+            return True
+        return False
+        
+    
 
 class ComputerController:
     def __init__(self):
         self.computers: dict[str, dict[str, Computer]] = {}
+        self.db = Database()
 
     def get_computer_by_name(self, username: str, name: str) -> Computer | APIError:
         if username in self.computers and name in self.computers[username]:
@@ -84,6 +128,8 @@ class ComputerController:
         name_error = validate_dict(action, "name")
         hash_key_error = (validate_dict(action, "name", "hash_key") or
                             self.check_computer_hash_key(action["username"], action["name"], action["hash_key"]))
+        computer = self.get_computer_by_name(action["username"], action["name"])
+        computer_error = computer if isinstance(computer, APIError) else None
         
         if method == Methods.CONNECT:
             if name_error:
@@ -94,10 +140,41 @@ class ComputerController:
 
             print(f"New hash key: {new_hash_key}")
 
-            computer = Computer(action["username"], action["name"], new_hash_key)
+            computer: Computer = Computer(action["username"], action["name"], new_hash_key)
+            self.computers[computer.username][computer.name] = computer
 
+            return {"result": True}
+
+        if method == Methods.PING:
+            return {"result": True}
+
+        if method == Methods.PING_ERROR:
+            return APIError.to_dict()
+
+        if method == Methods.BUTTON_ADD:
+            if hash_key_error:
+                return hash_key_error.to_dict()
+            if name_error:
+                return name_error.to_dict()
+            if computer_error:
+                return computer_error.to_dict()
+
+            computer.add_button(action["button_name"], action["button_text"])
+
+            return {"result": True}
+
+        if method == Methods.BUTTON_CLICK:
+            if hash_key_error:
+                return hash_key_error.to_dict()
+            if name_error:
+                return name_error.to_dict()
+            if computer_error:
+                return computer_error.to_dict()
+            
+            computer.click(action["button_name"])
+            
             return {"result": True}
 
 
 if __name__ == '__main__':  
-    print(Methods.find_and_validate({"method": "connect"}))
+    print(Methods.find_and_validate({"method": "computer.connect"}))
